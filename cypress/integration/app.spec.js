0e66a4dca463944af4c1ae2779a0e9d0e1793257
Aude Faucheux <aude53@gmail.com>
2020-06-22 16:17:14 +0100

    add cypress

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,18 @@
+describe("Visits the counter app", () => {
+  it("loads the home page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("A Useless App");
+    cy.contains("Counter");
+    cy.contains("Clock");
+  });
+
+  it("increments the counter", () => {
+    cy.contains("+").click();
+    cy.get(".count").should("have.text", "1");
+  });
+
+  it("decrements the counter", () => {
+    cy.contains("-").click();
+    cy.get(".count").should("have.text", "0");
+  });
+});
