4aa75d05a9e1741728ae4a5609c16247462003c1
Aude Faucheux <aude53@gmail.com>
2020-06-22 16:37:39 +0100

    add cypress to github actions

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders App title", () => {
   const { getByText } = render(<App />);
   expect(getByText("A Useless App")).toBeInTheDocument();
 });
